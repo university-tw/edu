d2d49f94b8a04c48b6cb7d1ff85e89944f54d2fe
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-11-20 01:45:12 +0800

    new site

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,13 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import {DefaultSeo} from "next-seo";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+    return <>
+        <DefaultSeo titleTemplate={'%s | A++程式設計 - University.tw 教育'}
+                    defaultTitle={'A++程式設計 - University.tw 教育'}
+                    description={'University.tw教育 打造符合新時代新課綱需求的課程，讓學生能夠在學習中獲得成就感'}
+        />
+        <Component {...pageProps} />
+    </>
 }
